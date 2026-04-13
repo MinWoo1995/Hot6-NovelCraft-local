ef7ff289d762afafa4509a97c5e34ae79c5289a6
MinWoo1995 <wjsalsdn120@naver.com>
2026-04-13 15:03:17 +0900

    First commit

---
+++ src/main/java/com/example/hot6novelcraft/Hot6NovelCraftApplication.java
@@ -0,0 +1,13 @@
+package com.example.hot6novelcraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hot6NovelCraftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hot6NovelCraftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hot6novelcraft/Hot6NovelCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hot6novelcraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hot6NovelCraftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
